bdae53a61d56376f354f31745eb5916858432518
ahkarminmyat <dev.ahkarminmyat98@gmail.com>
2024-07-16 13:49:13 +0700

    database maintainence'

---
+++ maintain-db.sql
@@ -0,0 +1,17 @@
+-- maintain db
+
+-- check table
+
+USE classicmodels
+
+CHECK TABLE customers
+
+-- analyze table
+
+ANALYZE TABLE employees 
+
+-- repair table
+REPAIR TABLE classicmodels.payments 
+
+-- optimize table
+OPTIMIZE TABLE classicmodels.productlines 
